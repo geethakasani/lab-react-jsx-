967eab7cb2a782e6cdd863d9da8df6a19c734005
geethakasani <pji.geethanjali.k@kalvium.community>
2024-05-28 22:12:37 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import './App.css';
-import imageData from './components/DataComponent';
+import elephant from "./images/elephant.jpeg";
+
+const imageData = ()=>{
+  let data = [
+    {
+      id:1,
+      img:elephant
+    },
+    {
+      id:2,
+      img:elephant
+    },
+    {
+      id:3,
+      img:elephant
+    },
+    {
+      id:4,
+      img:elephant
+    }
+  ]
+  return data;
+}
 
 function App() {
   // code here
-  return (
-    <div className='image-grid'>
-      {imageData.map((image)=>(
-        <div key={image.id} className='image-container'>
-          <img src={image.img} alt={'Elephant'}/>
+  return(
+    <div className="image-grid">
+      {imageData().map((image)=>(
+        <div key={image.id} className="image-container">
+          <img src={image.img} alt={`Elephant ${image.id}`}/>
+
         </div>
       ))}
 
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,16 +1,35 @@
 import { Component } from "react";
 import "./App.css"
-import imageData from "./components/DataComponent";
+import elephant from "./images/elephant.jpeg";
 
 export default class AppClass extends Component{
   
-  
-
+  imageData = ()=>{
+    let data = [
+      {
+        id:1,
+        img:elephant
+      },
+      {
+        id:2,
+        img:elephant
+      },
+      {
+        id:3,
+        img:elephant
+      },
+      {
+        id:4,
+        img:elephant
+      }
+    ]
+    return data;
+  }
   // code here
  render(){
   return(
     <div className="image-grid">
-      {imageData.map((image)=>(
+      {this.imageData().map((image)=>(
         <div key={image.id} className="image-container">
           <img src={image.img} alt={`Elephant ${image.id}`}/>
 
